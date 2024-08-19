2e4b07001c83607780d9d0df8fd6e1b04830dfeb
Marcos <marcos.ruiz@enti.cat>
2024-08-19 11:57:36 +0200

    Ya se hace grande la lista cuando se añade una nueva mierda

--- Assets/Project/Code/Scripts/List/ShopList.cs
+++ Assets/Project/Code/Scripts/List/ShopList.cs
@@ -9,19 +9,23 @@ public class ShopList : MonoBehaviour
 {
     [SerializeField] private float addItemTime;
     [SerializeField] private float distanceBetweenText;
+    [SerializeField] private float distanceInitText;
+    [SerializeField] private float distanceIncreaseShopList;
 
     [SerializeField] private GameObject newText;
+    [SerializeField] private GameObject shopList;
 
     private Dictionary<Item.Type, int> items = new Dictionary<Item.Type, int>();
 
     private float currentTime;
 
     private Vector2 currentPosition;
+    
 
     private void Start()
     {
         currentTime = 0;
-        currentPosition = new Vector2(transform.GetChild(0).localPosition.x, transform.GetChild(0).localPosition.y - distanceBetweenText * 2);
+        currentPosition = new Vector2(transform.GetChild(0).localPosition.x, transform.GetChild(0).localPosition.y - distanceInitText);
         GenerateItem();
     }
 
@@ -67,5 +71,6 @@ public class ShopList : MonoBehaviour
         itemText.GetComponent<TextMeshProUGUI>().text = newItem.type.ToString() + "...........x" + items[newItem.type];
 
         currentPosition.y = currentPosition.y - distanceBetweenText;
+        shopList.transform.localScale = new Vector3(shopList.transform.localScale.x, shopList.transform.localScale.y + distanceIncreaseShopList, shopList.transform.localScale.z);
     }
 }
